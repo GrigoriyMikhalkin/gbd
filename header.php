1587c0cf01cf5ece3e062e83a233598d62ac4e42
GrigoriyMikhalkin <mikgrigoriy@yandex.ru>
2015-08-18 22:43:23 +0300

    Cleanup

--- header.php
+++ header.php
@@ -3,14 +3,14 @@
 <head>
   <title>Get best deals on games!</title>
   <meta charset="utf-8" />
-  <link rel="stylesheet" href="http://localhost/greesha/gbd/stylesheets/header.css" />
+  <link rel="stylesheet" href="stylesheets/header.css" />
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   <script type="text/javascript" src="script.js"></script>
 </head>
 
 <body>
-  <a href="http://localhost/greesha/gbd/">
-  <img src="http://localhost/greesha/gbd/images/bestdealLogo.png" width="50%"></img></a>
+  <a href="index.php">
+  <img src="images/bestdealLogo.png" width="50%"></img></a>
 	<form>
 		<input type="text" name="gameName" id="name" >
 		<input id="submit" type="button" value="Submit">
